f60118a600260b83f1c61a584078866339103619
Martin Dousa <martindousa186@gmail.com>
2021-07-30 17:42:33 +0200

    refactoring

--- use_model.py
+++ use_model.py
@@ -3,28 +3,6 @@ import os
 import argparse
 from config_loader import Config
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--tokenizer_path", "-t", help="Path to tokenizer file", required=False, default="tokenizers/BLTokenizer.json", type=str)
-parser.add_argument("--model", "-m", help="Path to valid model folder", required=False, default="GPyT", type=str)
-
-args = parser.parse_args()
-
-assert os.path.exists(args.tokenizer_path) and os.path.isfile(args.tokenizer_path), "Invalid path to tokenizer file"
-assert os.path.exists(args.model) and os.path.isdir(args.model), "Invalid path to model folder"
-
-TOKENIZER = args.tokenizer_path
-MODEL_PATH = args.model
-
-tokenizer = PreTrainedTokenizerFast(tokenizer_file=TOKENIZER)
-tokenizer.add_special_tokens({
-  "eos_token": "</s>",
-  "bos_token": "<s>",
-  "unk_token": "<unk>",
-  "pad_token": "<pad>",
-  "mask_token": "<mask>"
-})
-
-model = GPT2LMHeadModel.from_pretrained(MODEL_PATH).to("cuda")
 
 def decode_newlines(inp):
   return inp.replace(Config.new_line_tag, "\n")
@@ -32,14 +10,14 @@ def decode_newlines(inp):
 def encode_newlines(inp):
   return inp.replace("\n", Config.new_line_tag)
 
-def auto_complete(inp):
+def auto_complete(inp, debug:bool=False):
   inp = encode_newlines(inp)
   newline_count = inp.count(Config.new_line_tag)
 
   input_ids = tokenizer.encode(inp, return_tensors="pt").to("cuda")
 
   model_output = model.generate(input_ids,
-                                max_length=Config.prediction_size, 
+                                max_length=Config.prediction_size if Config.prediction_size <= Config.n_ctx else Config.n_ctx,
                                 num_beams=Config.beam_count, 
                                 temperature=0.7, 
                                 no_repeat_ngram_size=5, 
@@ -50,10 +28,11 @@ def auto_complete(inp):
   sequence = model_output["sequences"][0]
   decoded = decode_newlines(tokenizer.decode(sequence))
 
-  # print("Debug whole decoded message:")
-  # print(40*"-")
-  # print(decoded)
-  # print(40*"-")
+  if debug:
+    print("Debug whole decoded message:")
+    print(40*"-")
+    print(decoded)
+    print(40*"-")
 
   auto_completed = ""
   split = decoded.split("\n")
@@ -62,10 +41,39 @@ def auto_complete(inp):
 
   return auto_completed
 
-while True:
-  try:
-    inp = input(">>> ")
-    print("\nAutocomplete:\n" + auto_complete(inp.rstrip()))
-  except KeyboardInterrupt:
-    break
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--tokenizer_path", "-t", help="Path to tokenizer file", required=False, default="tokenizers/BLTokenizer.json", type=str)
+  parser.add_argument("--model", "-m", help="Path to valid model folder", required=False, default="GPyT", type=str)
+  parser.add_argument("--debug", "-d", help="Show whole predicted output of model", action="store_true")
 
+  args = parser.parse_args()
+
+  assert os.path.exists(args.tokenizer_path) and os.path.isfile(args.tokenizer_path), "Invalid path to tokenizer file"
+  assert os.path.exists(args.model) and os.path.isdir(args.model), "Invalid path to model folder"
+
+  TOKENIZER = args.tokenizer_path
+  MODEL_PATH = args.model
+
+  tokenizer = PreTrainedTokenizerFast(tokenizer_file=TOKENIZER)
+  tokenizer.add_special_tokens({
+    "eos_token": "</s>",
+    "bos_token": "<s>",
+    "unk_token": "<unk>",
+    "pad_token": "<pad>",
+    "mask_token": "<mask>"
+  })
+
+  model = GPT2LMHeadModel.from_pretrained(MODEL_PATH).to("cuda")
+
+  while True:
+    try:
+      inp = input(">>> ")
+      if inp == "exit": exit(-1)
+      answer = auto_complete(inp.rstrip(), args.debug)
+      if not inp.startswith(" ") and answer.startswith(" "):
+        answer = answer[1:]
+
+      print("\nAutocomplete:\n" + answer)
+    except KeyboardInterrupt:
+      break
